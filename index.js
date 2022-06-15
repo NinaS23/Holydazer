3076e8530271aea3811eb3e1f8e028c7a7479131
MariaDuda <mariabevenuto062@gmail.com>
2022-06-15 11:35:29 -0300

    return the holidays

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from "cors"
+import holidays from "./holidays.js"
+
+const server = express()
+server.use(cors())
+
+console.log(holidays)
+server.get("/holidays", (req, resp) => {
+    resp.send(holidays)
+})
+
+server.get("/is-today-holiday", (req, resp) => {
+    const hoje = new Date().toLocaleDateString("en-US")
+    const feriado = 1/1/2022
+    let arr = []
+    holidays.map((holiday) => {
+        console.log(holiday.date)
+        arr.push(holiday.date)
+    })
+    const encontrar = arr.includes(feriado)
+    console.log(encontrar)
+    if (encontrar === true) {
+        resp.send("sim , é feriado")
+    } else {
+        resp.send("não , não é feriado")
+    }
+
+
+
+})
+
+
+server.listen(4000)
